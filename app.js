3f01ea484334b2a228b86061c9c29469b21054d5
EduardoFlores117 <eduardoflores121298@gmail.com>
2024-10-13 17:07:43 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path'); // Para gestionar rutas de archivos
 const morgan = require('morgan'); // Middleware para el registro de solicitudes
 const bodyParser = require('body-parser'); // Middleware para analizar cuerpos de solicitudes
-const express = require('express');
 const cors = require('cors');
 
 // Configurar CORS para permitir todos los orígenes
