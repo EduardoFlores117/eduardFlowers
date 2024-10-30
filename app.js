07c518aee5e679d3b1196b8badd2a407e057db82
EduardoFlores117 <eduardoflores121298@gmail.com>
2024-10-30 08:48:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 // Solo cargamos dotenv en desarrollo local
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();  // Esto solo lee .env si no estás en producción
+  require('dotenv').config();
 }
 
 const express = require('express');
-const path = require('path'); // Para gestionar rutas de archivos
-const morgan = require('morgan'); // Middleware para el registro de solicitudes
-const bodyParser = require('body-parser'); // Middleware para analizar cuerpos de solicitudes
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const authController = require('./controllers/authController'); // Importar el controlador de autenticación
-const authMiddleware = require('./middlewares/authMiddleware'); // Importar el middleware de autenticación
-
+const authController = require('./controllers/authController');
+const authMiddleware = require('./middlewares/authMiddleware');
 const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express'); // Importa Swagger UI
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 const comedorController = require('./controllers/comedorController');
 const employeeController = require('./controllers/EmployeeController');
 const attendanceStatController = require('./controllers/AttendanceStatController');
 
-// Definir el puerto y el host desde variables de entorno o usar valores predeterminados
 const PORT = process.env.PORT || 3000;
 const API_HOST = process.env.API_HOST || 'https://api-eduardflowers.onrender.com';
 
@@ -28,7 +26,7 @@ app.use(cors({
     origin: API_HOST
 }));
 
-// Middleware para servir archivos estáticos desde la carpeta 'public'
+// Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware para registrar solicitudes HTTP
@@ -36,7 +34,7 @@ app.use(morgan('dev'));
 
 // Middleware para analizar cuerpos de solicitudes JSON
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // Para formularios URL encoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configuración de Swagger
 const swaggerOptions = {
@@ -53,22 +51,37 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: API_HOST, // Usar la variable de entorno para el host
+                url: API_HOST,
                 description: 'Servidor de producción'
+            }
+        ],
+        components: {
+            securitySchemes: {
+                BearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                    description: "Ingrese el token en formato: Bearer <token>"
+                }
+            }
+        },
+        security: [
+            {
+                BearerAuth: [] // Esto aplica BearerAuth a todas las rutas de la API
             }
         ]
     },
-    apis: ['./controllers/*.js'], // Ruta donde Swagger buscará los comentarios JSDoc
+    apis: ['./controllers/*.js'],
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs)); // Ruta de Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Rutas de autenticación (sin protección)
-app.use('/api/auth', authController); // Rutas de registro e inicio de sesión
+app.use('/api/auth', authController);
 
 // Aplicar el middleware de autenticación a todas las rutas que comienzan con /api, excluyendo /api/auth
-app.use('/api', authMiddleware); // Este middleware se aplicará a las rutas que comienzan con /api, pero no afectará a /api/auth
+app.use('/api', authMiddleware);
 
 // Rutas del comedor
 app.get('/api/comedor', comedorController.getRecords);
